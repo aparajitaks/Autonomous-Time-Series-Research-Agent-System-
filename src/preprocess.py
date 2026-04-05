7012c171ae805b826c085c433c4645276d39748d
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-04-05 15:22:43 +0530

    Initial commit: add project files

---
+++ src/preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def preprocess_data(df):
+    df=df[['Date','Close']]
+    df['Date']=pd.to_datetime(df['Date'])
+    df=df.sort_values("Date")
+    df.set_index("Date",inplace=True)
+    df.rename(columns={"Date":y},inplace=True)
+    return df
+    
+
+
+
+
+           
